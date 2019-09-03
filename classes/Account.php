660f56e7522f971cba324f37d23e5436c75330ba
Xavier Trinidad <xmtrinidad01@gmail.com>
2019-09-03 10:05:36 -0700

    Refactored errors into associative array

--- classes/Account.php
+++ classes/Account.php
@@ -2,6 +2,16 @@
     class Account {
         private $errorArray;
 
+        private $passwordsDoNotMatch = "Your passwords don't match";
+        private $passwordNotAlphanumeric = "Your passwords can only contain numbers and letters";
+        private $passwordCharacters = "Your username must be between 5 and 30 characters";
+        private $emailInvalid = "Email is invalid";
+        private $emailsDoNotMatch = "Your emails don't match";
+        private $lastNameCharacters = "Your Last Name must be between 2 and 25 characters";
+        private $firstNameCharacters = "Your First Name must be between 2 and 25 characters";
+        private $usernameCharacters = "Your username must be between 3 and 25 characters";
+        
+
         public function __construct() {
             $this->errorArray = array();
         }
@@ -28,7 +38,7 @@
 
         private function validateUsername($un) {
             if(strlen($un) > 25 || strlen($un) < 3) {
-                array_push($this->errorArray, "Your username must be between 3 and 25 characters");
+                $this->errorArray['usernameCharacters'] = $this->usernameCharacters;
                 return;
             }
 
@@ -37,26 +47,26 @@
 
         private function validateFirstName($fn) {
             if(strlen($fn) > 25 || strlen($fn) < 2) {
-                array_push($this->errorArray, "Your First Name must be between 2 and 25 characters");
+                $this->errorArray['firstNameCharacters'] = $this->firstNameCharacters;
                 return;
             }
         }
 
         private function validateLastName($ln) {
             if(strlen($ln) > 25 || strlen($ln) < 2) {
-                array_push($this->errorArray, "Your Last Name must be between 2 and 25 characters");
+                $this->errorArray['lastNameCharacters'] = $this->lastNameCharacters;
                 return;
             }
         }
 
         private function validateEmails($em, $em2) {
             if ($em != $em2) {
-                array_push($this->errorArray, "Your emails don't match");
+                $this->errorArray['emailsDoNotMatch'] = $this->emailsDoNotMatch;
                 return;
             }
 
             if (!filter_var($em, FILTER_VALIDATE_EMAIL)) {
-                array_push($this->errorArray, "Email is invalid");
+                $this->errorArray['emailInvalid'] = $this->emailInvalid;
                 return;
             }
 
@@ -65,17 +75,17 @@
 
         private function validatePasswords($pw, $pw2) {
             if ($pw != $pw2) {
-                array_push($this->errorArray, "Your passwords don't match");
+                $this->errorArray['passwordsDoNotMatch'] = $this->passwordsDoNotMatch;
                 return;
             }
 
             if (preg_match('/[^A-Za-z0-9]/', $pw)) {
-                array_push($this->errorArray, "Your passwords can only contain numbers and letters");
+                $this->errorArray['passwordNotAlphanumeric'] = $this->passwordNotAlphanumeric;
                 return;
             }
 
             if(strlen($pw) > 30 || strlen($pw) < 5) {
-                array_push($this->errorArray, "Your username must be between 5 and 30 characters");
+                $this->errorArray['passwordCharacters'] = $this->passwordCharacters;
                 return;
             }
         }
